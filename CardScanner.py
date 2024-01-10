158d22974c2837a8d7eab84928dd073aa4ec5b52
BabyMil <emilkaangenc@gmail.com>
2024-01-10 12:05:33 +0100

    brrg

--- CardScanner.py
+++ CardScanner.py
@@ -11,99 +11,77 @@
 #define GREEN_LED_PIN   7  // Change to the digital pin for your green LED
 #define RED_LED_PIN     6  // Change to the digital pin for your red LED
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
 
-#Define the UIDs of the allowed cards
-byte allowedCardUIDs[][4] = {
-  {0x00, 0x7A, 0x71, 0x1A},
-  {0x83, 0x84, 0x83, 0x08},
-  {0x00, 0x83, 0x80, 0x1A}
-};
+RST_PIN = 9
+SS_PIN = 10
 
-#Define the welcome messages for each card
-const char* welcomeMessages[] = {
-  "Welcome Kaan!",
-  "Welcome Mattias!",
-  "Welcome Jessica!"
-};
+GREEN_LED_PIN = 7  # Change to the GPIO pin for your green LED
+RED_LED_PIN = 6  # Change to the GPIO pin for your red LED
 
-void setup() {
-  Serial.begin(9600);
-  SPI.begin();
-  mfrc522.PCD_Init();
-  
-  pinMode(GREEN_LED_PIN, OUTPUT);
-  pinMode(RED_LED_PIN, OUTPUT);
-  
-  Serial.println("RFID Reader Initialized");
-}
+allowed_card_uids = [
+    [0x00, 0x7A, 0x71, 0x1A],
+    [0x83, 0x84, 0x83, 0x08],
+    [0x00, 0x83, 0x80, 0x1A]
+]
 
-void loop() {
-  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-    Serial.print("Card UID: ");
-    for (byte i = 0; i < mfrc522.uid.size; i++) {
-      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-      Serial.print(mfrc522.uid.uidByte[i], HEX);
-    }
+welcome_messages = [
+    "Welcome Kaan!",
+    "Welcome Mattias!",
+    "Welcome Jessica!"
+]
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(GREEN_LED_PIN, GPIO.OUT)
+    GPIO.setup(RED_LED_PIN, GPIO.OUT)
+
+    reader = SimpleMFRC522()
     
-    if (checkCardUID()) {
-      Serial.print(" - ");
-      Serial.println(welcomeMessages[getCardIndex()]);
-      turnOnGreenLED();
-      // Add your custom actions for this card here
-    } else {
-      Serial.println(" - Access Denied");
-      turnOnRedLED();
-    }
+    print("RFID Reader Initialized")
+    return reader
 
-    delay(1000);  // Add a delay to avoid reading the same card repeatedly
-    turnOffLEDs();
-  }
-}
+def loop(reader):
+    try:
+        print("Place a card near the reader...")
+        while True:
+            id, card_uid = reader.read()
+            print("Card UID: ", card_uid)
 
-bool checkCardUID() {
-  // Compare the scanned card's UID with the allowed card UIDs
-  for (byte j = 0; j < sizeof(allowedCardUIDs) / sizeof(allowedCardUIDs[0]); j++) {
-    bool match = true;
-    for (byte i = 0; i < mfrc522.uid.size; i++) {
-      if (mfrc522.uid.uidByte[i] != allowedCardUIDs[j][i]) {
-        match = false;
-        break;
-      }
-    }
-    if (match) {
-      return true;  // Card UID matches one of the allowed cards
-    }
-  }
-  return false;  // Card UID doesn't match any allowed cards
-}
+            if check_card_uid(card_uid):
+                print(" - ", welcome_messages[get_card_index(card_uid)])
+                turn_on_green_led()
+                # Add your custom actions for this card here
+            else:
+                print(" - Access Denied")
+                turn_on_red_led()
 
-byte getCardIndex() {
-  // Get the index of the matched card in the allowedCardUIDs array
-  for (byte j = 0; j < sizeof(allowedCardUIDs) / sizeof(allowedCardUIDs[0]); j++) {
-    bool match = true;
-    for (byte i = 0; i < mfrc522.uid.size; i++) {
-      if (mfrc522.uid.uidByte[i] != allowedCardUIDs[j][i]) {
-        match = false;
-        break;
-      }
-    }
-    if (match) {
-      return j;  // Return the index of the matched card
-    }
-  }
-  return 255;  // Return an invalid index if no match is found
-}
+            GPIO.cleanup()  # Reset GPIO state to avoid interference with other programs
 
-void turnOnGreenLED() {
-  digitalWrite(GREEN_LED_PIN, HIGH);
-}
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
-void turnOnRedLED() {
-  digitalWrite(RED_LED_PIN, HIGH);
-}
+def check_card_uid(card_uid):
+    # Compare the scanned card's UID with the allowed card UIDs
+    for allowed_uid in allowed_card_uids:
+        if card_uid == allowed_uid:
+            return True  # Card UID matches one of the allowed cards
+    return False  # Card UID doesn't match any allowed cards
 
-void turnOffLEDs() {
-  digitalWrite(GREEN_LED_PIN, LOW);
-  digitalWrite(RED_LED_PIN, LOW);
-}
+def get_card_index(card_uid):
+    # Get the index of the matched card in the allowed_card_uids array
+    for j, allowed_uid in enumerate(allowed_card_uids):
+        if card_uid == allowed_uid:
+            return j  # Return the index of the matched card
+    return 255  # Return an invalid index if no match is found
+
+def turn_on_green_led():
+    GPIO.output(GREEN_LED_PIN, GPIO.HIGH)
+
+def turn_on_red_led():
+    GPIO.output(RED_LED_PIN, GPIO.HIGH)
+
+if __name__ == "__main__":
+    reader = setup()
+    loop(reader)
