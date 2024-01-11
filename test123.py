ca9cc0655dd01729e0642677201442a51072d452
Ekkodrom <you@example.com>
2024-01-11 13:40:58 +0100

    123

--- test123.py
+++ test123.py
@@ -73,7 +73,7 @@ def welcome_sound():
    # print("Goodbye!")
    # play_sound("check.mp3")
     
-def fetchEvents():
+def fetchEvents(calendar_key):
     start_date = datetime.now()
     end_date = start_date + timedelta(days=7)
 
@@ -82,6 +82,7 @@ def fetchEvents():
         'endDate': end_date.strftime('%Y-%m-%d')
     }
 
+    request_url = f"{api_url}/{calendar_key}/events"
     response = requests.get(request_url, headers=headers, params=params)
     if response.status_code == 200:
         return json.loads(response.text)['events']
@@ -89,12 +90,11 @@ def fetchEvents():
         print("Failed to fetch events")
         return []
 
-
 def showCalendar(events):
     gui = Tk()
     gui.config(background='grey')
     gui.title("Teamup Calendar")
-    gui.attributes("-fullscreen", True)
+    gui.attributes("-fullscreen", True) 
 
     # Frame for the Log Off button
     top_frame = Frame(gui, bg='grey')
@@ -118,6 +118,17 @@ def showCalendar(events):
     canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
     canvas.create_window((0, 0), window=scrollable_frame, anchor='nw')
 
+    # Track the drag start point
+    def start_scroll(event):
+        canvas.scan_mark(event.x, event.y)
+
+    # Perform the scrolling
+    def perform_scroll(event):
+        canvas.scan_dragto(event.x, event.y, gain=1)
+
+    canvas.bind("<Button-1>", start_scroll)
+    canvas.bind("<B1-Motion>", perform_scroll)
+
     # Adding events to the scrollable_frame
     for event in events:
         start_time = datetime.fromisoformat(event['start_dt']).strftime("%A, %B %d, %Y %H:%M")
@@ -135,18 +146,23 @@ def showCalendar(events):
 
     gui.mainloop()
 
-
-if __name__=='__main__':
-    # Define your Teamup API URL and API key
+if __name__ == '__main__':
     api_url = "https://api.teamup.com"
     api_key = "699e02c0555e1804ea722d893851875e8444e8bf17199c8d8e46bc393a60f960"
-    calendar_key = "kskp2dg3mpgu24n3ww"
-    request_url = f"{api_url}/{calendar_key}/events"
+
+    # Dictionary mapping card IDs to calendar keys
+    card_calendar_map = {
+        '2054232593': 'kskp2dg3mpgu24n3ww',
+        '2206210585': 'ks2yz86rfe8sj5nvq1',
+        # Add more card IDs and their corresponding calendar keys
+    }
+
     headers = {"Teamup-Token": api_key}
 
-    events = fetchEvents()
-    showCalendar(events)
-
+    for card_id, calendar_key in card_calendar_map.items():
+        events = fetchEvents(calendar_key)
+        showCalendar(events)
+        
 def read_rfid():
     reader = SimpleMFRC522()
 
