967dbcc3e48bac5b846e3a51c8ce7d0fdab0ba27
PloxyP <mattias.bonnesen@gmail.com>
2024-01-12 15:41:33 +0100

    bingo

--- Mtest2.py
+++ Mtest2.py
@@ -1,7 +1,6 @@
 import time
 
-def thread_function_2():
-    global shared_bool
+def thread_function_2(shared_bool):
     while True:
         time.sleep(1)
         print(f"Thread 2: {shared_bool}")
