5cac2f1d5e2e4788fe111efa4fcad46a4d441a3f
Sayocho <jessicatittes@gmail.com>
2024-01-12 16:16:57 +0100

    as

--- ttest123.py
+++ ttest123.py
@@ -33,6 +33,76 @@ def goodbye_sound():
 face_cascade = cv2.CascadeClassifier('/home/gruppesjov/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('/home/gruppesjov/opencv/data/haarcascades/haarcascade_eye.xml')
 
+# def face_detection():
+#     global eyes_detected
+#     global looking_at_camera
+#     global played_sound
+    
+#     while True:
+#         ret, frame = cap.read()
+
+#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+#         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+#         if len(faces) == 0:
+#             looking_at_camera = False
+#             played_sound = False  # Reset the flag when no faces are detected
+
+#         for (x, y, w, h) in faces:
+#             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+#             roi_gray = gray[y:y+w, x:x+w]
+#             roi_color = frame[y:y+h, x:x+w]
+#             eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 8)
+
+#             for (ex, ey, ew, eh) in eyes:
+#                 cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
+#                 looking_at_camera = True
+#                 eyes_detected = True
+#                 threading.Thread(target=welcome_message, args=(eyes_detected,)).start()  # Start the thread
+# # def face_detection():
+# #     global eyes_detected
+    
+# #     while True:
+# #         ret, frame = cap.read()
+
+# #         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+# #         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+# #         if len(faces) == 0:
+# #             looking_at_camera = False
+# #             played_sound = False  # Reset the flag when no faces are detected
+
+# #         for (x, y, w, h) in faces:
+# #             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+# #             roi_gray = gray[y:y+w, x:x+w]
+# #             roi_color = frame[y:y+h, x:x+w]
+# #             eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 8)
+
+# #             for (ex, ey, ew, eh) in eyes:
+# #                 cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
+# #                 looking_at_camera = True
+# #                 eyes_detected = True
+# #                 event = threading.Event()
+# #                 threading.Thread(target=welcome_message, args=(eyes_detected,))
+
+#         #cv2.imshow('frame', frame)
+        
+#         # Play sounds based on the flag and ensure it's played only once
+#         if looking_at_camera and not played_sound:
+#             welcome_sound()
+#             played_sound = True
+
+#         if not looking_at_camera and played_sound:
+#             goodbye_sound()
+#             played_sound = False
+        
+#         if cv2.waitKey(1) == ord('q'):
+#             break
+
+#     cap.release()
+#     cv2.destroyAllWindows()
 def face_detection():
     global eyes_detected
     global looking_at_camera
@@ -60,35 +130,7 @@ def face_detection():
                 looking_at_camera = True
                 eyes_detected = True
                 threading.Thread(target=welcome_message, args=(eyes_detected,)).start()  # Start the thread
-# def face_detection():
-#     global eyes_detected
-    
-#     while True:
-#         ret, frame = cap.read()
 
-#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-#         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-#         if len(faces) == 0:
-#             looking_at_camera = False
-#             played_sound = False  # Reset the flag when no faces are detected
-
-#         for (x, y, w, h) in faces:
-#             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
-#             roi_gray = gray[y:y+w, x:x+w]
-#             roi_color = frame[y:y+h, x:x+w]
-#             eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 8)
-
-#             for (ex, ey, ew, eh) in eyes:
-#                 cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
-#                 looking_at_camera = True
-#                 eyes_detected = True
-#                 event = threading.Event()
-#                 threading.Thread(target=welcome_message, args=(eyes_detected,))
-
-        #cv2.imshow('frame', frame)
-        
         # Play sounds based on the flag and ensure it's played only once
         if looking_at_camera and not played_sound:
             welcome_sound()
@@ -103,6 +145,7 @@ def face_detection():
 
     cap.release()
     cv2.destroyAllWindows()
+
 
 def play_sound(file_path):
     pygame.mixer.init()
