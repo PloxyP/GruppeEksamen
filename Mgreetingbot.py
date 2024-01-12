c9ca7406d2cb8a02d7770ec64de30d30a64a6cc7
PloxyP <mattias.bonnesen@gmail.com>
2024-01-13 00:03:34 +0100

    sadfsafd

--- Mgreetingbot.py
+++ Mgreetingbot.py
@@ -149,10 +149,10 @@ def rfid_function():
 
             if card_id in card_calendar_map:
                 calendar_key = card_calendar_map[card_id]
-                welcome_sound()
                 print(f"Fetching events for calendar key: {calendar_key}")
                 events = fetchEvents(api_url, headers, calendar_key)
                 if events:
+                    welcome_sound()
                     showCalendar(events)
                 else:
                     print("No events found or error in fetching events")
