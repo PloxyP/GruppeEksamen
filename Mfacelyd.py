7574c999e418c1213da20584f0ecfa9f16821e7c
PloxyP <mattias.bonnesen@gmail.com>
2024-01-18 11:06:56 +0100

    dfsdfdsfdfsfds

--- Mfacelyd.py
+++ Mfacelyd.py
@@ -1,41 +1,43 @@
-import os
 import cv2
 from multiprocessing import Process, Value
 from Mwelcome import welcome_message
 from Mgreetingbot import rfid_function
 import RPi.GPIO as GPIO
 
-# Shared variable to signal eyes detections
-eyes_detected = Value('b', False)  # Initial value
-KortGodkendt = Value('b', False)
-KortScannet = Value('b', False)
-ExitGUI = Value('b', False)
+#Delte variabler til multiprocessing
+eyes_detected = Value('b', False)       #Variabel for om øjne er detekteret i Mfacelyd.py
+KortGodkendt = Value('b', False)        #Variabel for om kort er godkendt i Mgreetingbot.py
+KortScannet = Value('b', False)         #Variabel for om kort er scannet i Mgreetingbot.py
+ExitGUI = Value('b', False)             #Variabel for om GUI bliver exittet i Mgreetingbot.py
 
-cap = cv2.VideoCapture(0)
+#Open CV functioner:
+cap = cv2.VideoCapture(0)               #Index for valg af camera
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-cap.set(cv2.CAP_PROP_FPS, 30)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  #Sat billede bredde
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) #Sat billede højde
+cap.set(cv2.CAP_PROP_FPS, 30)           #Sat billeder per sekund
 
-looking_at_camera = False
-led_on = False
-led_pin = 24  # GPIO pin for the LED, change it to your actual pinn
+#Globale variabler
+face_detected = False               #Variabel for om ansigt er detekteret
+led_on = False                          #Pin on/off check
+led_pin = 24                            #GPIO pin (Ikke fysisk pin nummer)
 
-# Initialize GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(led_pin, GPIO.OUT)
-GPIO.output(led_pin, GPIO.LOW)
+#GPIO startup:
+GPIO.setmode(GPIO.BCM)                  #Pin nummer sat til GPIO værdi.
+GPIO.setup(led_pin, GPIO.OUT)           #Pin mode sat til output
+GPIO.output(led_pin, GPIO.LOW)          #Start værdi sat som low(off)
 
+#Function til at tænde LED
 def welcome_led():
-    print("Welcome! - LED on")
     GPIO.output(led_pin, GPIO.HIGH)
 
+#Function til at slukke LED
 def goodbye_led():
-    print("Goodbye! - LED off")
     GPIO.output(led_pin, GPIO.LOW)
 
+#Main function (Køre fra starten i multiprocess)
 def face_detection(eyes_detected):
-    global looking_at_camera, led_on
+    global face_detected, led_on
     
     while True:
         ret, frame = cap.read()
@@ -45,9 +47,9 @@ def face_detection(eyes_detected):
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
         if len(faces) == 0:
-            looking_at_camera = False
+            face_detected = False
         else:
-            looking_at_camera = True
+            face_detected = True
 
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
@@ -63,11 +65,11 @@ def face_detection(eyes_detected):
         #cv2.imshow('frame', frame)
 
         #Turns on LED
-        if looking_at_camera == True and led_on == False:
+        if face_detected == True and led_on == False:
             welcome_led()
             led_on = True
 
-        if looking_at_camera == False and led_on == True:
+        if face_detected == False and led_on == True:
             goodbye_led()
             led_on = False
         
@@ -77,8 +79,8 @@ def face_detection(eyes_detected):
     cap.release()
     cv2.destroyAllWindows()
 
+#Main statement:
 if __name__ == "__main__":
-    eyes_detected = Value('b', False)  # Initial value
     face_cascade = cv2.CascadeClassifier('/home/gruppesjov/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier('/home/gruppesjov/opencv/data/haarcascades/haarcascade_eye.xml')
     
