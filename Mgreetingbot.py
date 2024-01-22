9506027ada5693fdd088e95b4b5f1fb4ec6eac94
Ekkodrom <you@example.com>
2024-01-22 13:00:44 +0100

    frankoghans

--- Mgreetingbot.py
+++ Mgreetingbot.py
@@ -88,9 +88,9 @@ def showCalendar(events, ExitGUI):                      #GUI funktion der laver 
         ExitGUI.value = True                            #Bool som ændre statement i multiprocessing
         time.sleep(2)                                   #Tid til multiprocessing at reagerer
         gui.destroy()                                   #luk kalender layout ned
-        # GPIO.setmode(GPIO.BCM)                          #reset lyset op igen
-        # GPIO.setup(24, GPIO.OUT)
-        # GPIO.output(24, GPIO.LOW)
+        GPIO.setmode(GPIO.BCM)                          #reset lyset op igen
+        GPIO.setup(24, GPIO.OUT)
+        GPIO.output(24, GPIO.LOW)
 
 
 #----------------------------------------Log Off Layout ----------------------------------------------------#
@@ -160,9 +160,9 @@ def read_rfid(reader, channel, card_calendar_map):
         else:
             # The ID is not in the dictionary
             print(f"No calendar key found for ID {card_id}")
-            # GPIO.setmode(GPIO.BCM)                          #reset lyset op igen
-            # GPIO.setup(24, GPIO.OUT)
-            # GPIO.output(24, GPIO.LOW)
+            GPIO.setmode(GPIO.BCM)                          #reset lyset op igen
+            GPIO.setup(24, GPIO.OUT)
+            GPIO.output(24, GPIO.LOW)
 
         # Update the total reads count
         total_reads += 1
